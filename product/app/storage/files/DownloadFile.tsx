e72dade7d10635c79f83f9abc6e084da1f6cceb7
Rodion Chachura <geekrodion@gmail.com>
2024-12-21 14:08:22 +0400

    add file download utility and refactor DownloadFile component to use it

--- product/app/storage/files/DownloadFile.tsx
+++ product/app/storage/files/DownloadFile.tsx
@@ -4,6 +4,7 @@ import { DownloadIcon } from '@lib/ui/icons/DownloadIcon'
 import { useMutation } from '@tanstack/react-query'
 import { useAutoDriveApi } from '../state/autoDriveApi'
 import { useCurrentFile } from './state/currentFile'
+import { initiateFileDownload } from '@lib/ui/utils/initiateFileDownload'
 
 export const DownloadFile = () => {
   const { name, headCid, type } = useCurrentFile()
@@ -18,17 +19,7 @@ export const DownloadFile = () => {
         file = Buffer.concat([file, chunk])
       }
 
-      const blob = new Blob([file], { type })
-
-      const url = URL.createObjectURL(blob)
-      const a = document.createElement('a')
-      a.href = url
-      a.download = name ?? headCid
-      document.body.appendChild(a)
-      a.click()
-
-      a.remove()
-      URL.revokeObjectURL(url)
+      initiateFileDownload({ type, value: file, name: name ?? headCid })
     },
   })
 
